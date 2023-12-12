23b12e254edda0710b1629fc79c69dd6335a1ea1
Kate Romashko <laurenkateryna@gmail.com>
2023-12-12 17:58:59 +0200

    homework 6

---
+++ homework-lesson-6/script6-1.js
@@ -0,0 +1,41 @@
+function reverseArray(arr) {
+  return arr.reverse();
+}
+
+const originalArray = [1, 2, 3, 4, 5];
+const reversedArray = reverseArray(originalArray);
+
+console.log(reversedArray);
+
+function uniqueValues(arr1, arr2) {
+  let arr = arr1.concat(arr2);
+  let result = [];
+
+  for (let el of arr) {
+    if (!result.includes(el)) {
+      result.push(el);
+    }
+  }
+
+  return result;
+}
+
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [3, 4, 5, 6, 7];
+const uniqueValuesArray = uniqueValues(array1, array2);
+
+console.log(uniqueValuesArray);
+
+function calculateAverageGrade(persons) {
+  let averageGrade =
+    persons.reduce((prev, person) => prev + person.grade, 0) / persons.length;
+  return averageGrade.toFixed(1);
+}
+
+const students = [
+  { name: "Alice", age: 20, grade: 4.5 },
+  { name: "Bob", age: 21, grade: 3.9 },
+  { name: "Charlie", age: 19, grade: 4.8 },
+];
+
+console.log(calculateAverageGrade(students));
